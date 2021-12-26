33d7bc4e53d904a9eef5632819967d9ccea1f7e5
Michael Pace <michael.pace@flowhub.com>
2021-12-26 08:49:42 -0700

    Load schedule programmatically from scene delegate

---
+++ LunchTime/ScheduleListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ScheduleListViewController.swift
+//  LunchTime
+//
+//  Created by jarvis on 12/26/21.
+//
+//
+
+import UIKit
+
+struct DailyMenu {
+
+}
+
+class ScheduleListViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+        return tableView
+    }()
+
+    private var dailyMenus: [DailyMenu] = []
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    override func loadView() {
+        super.loadView()
+
+        view = tableView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ScheduleListViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dailyMenus.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "DailyMenuCell")
+        return cell
+    }
+}
